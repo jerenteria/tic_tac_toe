03f3d3d07a46bd8506e3c916443f60b42b19d132
jerenteria <renteria.juan96@gmail.com>
2021-12-20 19:43:54 -0800

    finished

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import random
+
 # stores the board
-board = [' ', for x in range(10)]
+board = [' ' for x in range(10)]
 
 def insertLetter(letter, pos):
     board[pos] = letter
@@ -21,14 +23,7 @@ def printBoard(board):
     print('   |   |')
 
 def isWinner(bo, le):
-    return (bo[7] == le and bo[8] == le and bo[9] == le) or
-    (bo[4] == le and bo[5] == le and bo[6] == le) or
-    (bo[1] == le and bo[2] == le and bo[3] == le) or
-    (bo[1] == le and bo[4] == le and bo[7] == le) or
-    (bo[2] == le and bo[5] == le and bo[8] == le) or
-    (bo[3] == le and bo[6] == le and bo[6] == le) or
-    (bo[1] == le and bo[5] == le and bo[9] == le) or
-    (bo[3] == le and bo[5] == le and bo[7] == le) or
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or(bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or (bo[1] == le and bo[4] == le and bo[7] == le) or (bo[2] == le and bo[5] == le and bo[8] == le) or (bo[3] == le and bo[6] == le and bo[6] == le) or (bo[1] == le and bo[5] == le and bo[9] == le) or (bo[3] == le and bo[5] == le and bo[7] == le)
 
 def playerMove():
     run = True
@@ -50,34 +45,78 @@ def playerMove():
 
 
 def compMove():
-    pass
+    # enumerate gives all indeces and actual values of items in list
+    # check if the letter is blank and the indece is not 0 
+    # computer cannot play position 0
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+    # first check for O's and X's
+    for let in ['O', 'X']:
+        # look for all the possible moves first
+        for i in possibleMoves:
+            # make a new copy of the board
+            boardCopy = board[:]
+            # place a letter in our copy of the board
+            boardCopy[i] = let
+            # check if we won with that letter/move
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1, 3, 7, 9]:
+            cornersOpen.append(i)
+    
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+    
+    if 5 in possibleMoves:
+        move - 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2, 4, 6, 8]:
+            edgesOpen.append(i)
+    
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        return move
 
 def selectRandom(board):
-    pass
+    ln = len(li)
+    r = random.randrange(0, ln)
+    return li[r]
 
 def isBoardFull(board):
     if(board.count(' ') > 1):
-        return True
-    else:
         return False
+    else:
+        return True
 
 def main():
     print("Welcome to Tic Tac Toe!")
-    printBoard()
+    printBoard(board)
 
     while not(isBoardFull(board)):
         # if Os is the winner then no need for a player move
         if not(isWinner(board, 'O')):
             playerMove()
-            printBoard()
+            printBoard(board)
         else:
             print("Sorry, O\'s won this game!")
             break
 
         # if Xs is the winner then no need for a player move
-        if not(isWinner(board, 'O')):
-            compMove()
-            printBoard()
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Tie Game!')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move, ':')
+                printBoard(board)
         else:
             print("X\'s won this game! Good Job!")
             break
